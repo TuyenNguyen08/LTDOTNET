daa8bca47e327d7a6383fcef3b2893b302f72334
Le Nguyen Gia Hung <lenguyengiahung@gmail.com>
2019-02-26 13:57:24 +0700

    Fix menu gen Url by database config

--- HospitalWeb/Hospital.Web/EfModels/Menu.cs
+++ HospitalWeb/Hospital.Web/EfModels/Menu.cs
@@ -18,6 +18,9 @@ namespace Hospital.Web.EfModels
         public int FkNgonNgu { get; set; }
         public bool? Root { get; set; }
         public string PageName { get; set; }
+        public string ControllerName { get; set; }
+        public string ActionName { get; set; }
+        public string ParamId { get; set; }
 
         public virtual NgonNgu FkNgonNguNavigation { get; set; }
         public virtual ICollection<Page> Page { get; set; }
