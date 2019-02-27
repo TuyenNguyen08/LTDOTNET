12c14ebfcfd676870e01b4cacb0225f1d7b70394
Le Nguyen Gia Hung <lenguyengiahung@gmail.com>
2019-02-27 11:01:45 +0700

    Update kendo UI, fix bug _Layout, add ImageBrowser

---
+++ HospitalWeb/Hospital.Web/Controllers/ImageBrowserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital.Web.Controllers
+{
+    public class ImageBrowserController : Controller
+    {
+        private string _destination = @"D:\FolderImageBenhVien";
+
+        [Route("ImageBrowser/GetImage/{id}/image")]
+        public IActionResult GetImage(string id)
+        {
+            var file = System.IO.Path.Combine(_destination, id);
+
+            var provider = new Microsoft.AspNetCore.StaticFiles.FileExtensionContentTypeProvider();
+            string contentType;
+            if (!provider.TryGetContentType(file, out contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+
+            return File(new System.IO.FileStream(file, System.IO.FileMode.Open), contentType);
+        }
+    }
+}
